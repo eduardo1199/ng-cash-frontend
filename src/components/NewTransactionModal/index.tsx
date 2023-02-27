89a5874db8730176150df56cc880ae8214347786
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-02-27 17:57:11 -0300

    styled buttons of the types income and outcome

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from 'phosphor-react'
-import { CloseButton, Content, Overlay } from './styles'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionsType,
+  TransactionsTypeButton,
+} from './styles'
 
 export function NewTransactionModal() {
   return (
@@ -18,6 +24,17 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Descrição" required />
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <TransactionsType>
+            <TransactionsTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              Guardar
+            </TransactionsTypeButton>
+            <TransactionsTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              Transferir
+            </TransactionsTypeButton>
+          </TransactionsType>
 
           <button type="submit">Cadastrar</button>
         </form>
