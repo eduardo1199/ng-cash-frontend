b9dbf60ab915bb2229f1feaca50bfbba84e01373
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-03-02 11:17:23 -0300

    added validation forms with zod and useform

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,13 +1,39 @@
 import { SearchFormContainer } from './styles'
 
 import { MagnifyingGlass } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const searchSchemaForm = z.object({
+  query: z.string(),
+})
+
+type SearchFormInput = z.infer<typeof searchSchemaForm>
 
 export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInput>({
+    resolver: zodResolver(searchSchemaForm),
+  })
 
-      <button type="submit">
+  function handleSearchTransitions(data: SearchFormInput) {
+    console.log(data)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransitions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
