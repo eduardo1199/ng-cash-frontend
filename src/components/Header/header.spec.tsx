450e5b9c0221aad4ea01690c5ff8001b96c99fbd
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-03-20 09:00:33 -0300

    testing component header

--- src/components/Header/header.spec.tsx
+++ src/components/Header/header.spec.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react'
-
+import userEvent from '@testing-library/user-event'
 import { Header } from './index'
 
 jest.mock('react-router-dom', () => {
@@ -10,10 +10,47 @@ jest.mock('react-router-dom', () => {
   }
 })
 
-describe('Test component header', () => {
-  it('should be rendered button new Transaction', () => {
-    const { debug } = render(<Header />)
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
 
-    debug()
+describe('Test component header', () => {
+  window.ResizeObserver = ResizeObserver
+
+  it('should be rendered button new Transaction', async () => {
+    const { findByTitle } = render(<Header />)
+
+    const NewTransactionButton = await findByTitle('new-transaction')
+
+    expect(NewTransactionButton).toBeInTheDocument()
+  })
+
+  it('should be rendered button logout', async () => {
+    const { findByTitle } = render(<Header />)
+
+    const LogoutButton = await findByTitle('logout')
+
+    expect(LogoutButton).toBeInTheDocument()
+  })
+
+  it('should be not rendered new transaction modal', () => {
+    const { queryByTitle } = render(<Header />)
+
+    const TransactionForm = queryByTitle('new-transaction-form')
+
+    expect(TransactionForm).not.toBeInTheDocument()
+  })
+
+  it('should be rendered new transaction modal', async () => {
+    const { findByTitle } = render(<Header />)
+
+    const NewTransactionButton = await findByTitle('new-transaction')
+
+    await userEvent.click(NewTransactionButton)
+
+    const TransactionForm = await findByTitle('new-transaction-form')
+    expect(TransactionForm).toBeInTheDocument()
   })
 })
